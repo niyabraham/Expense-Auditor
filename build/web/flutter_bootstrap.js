46e234294f910c055df576b0c694813e0c5dc24d
niyabraham <niyaabraham06@gmail.com>
2026-04-10 14:23:34 +0530

    rebuild: js_util.globalThis PDF fix compiled in

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3051906241"
+    serviceWorkerVersion: "209228440"
   }
 });
