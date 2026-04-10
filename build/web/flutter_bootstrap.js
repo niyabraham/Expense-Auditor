61477265a821f0489198833c39a5a9238a343147
niyabraham <niyaabraham06@gmail.com>
2026-04-10 11:43:23 +0530

    fix: load PDF.js on-demand to prevent renderPdfPageToBase64Png not found

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2528338187"
+    serviceWorkerVersion: "3314850376"
   }
 });
